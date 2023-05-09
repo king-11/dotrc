5ebe0fb7324c9353a2bcaa08cd6f3ef1984ee89b
Lakshya Singh <lakshay.singh1108@gmail.com>
2023-05-09 11:39:59 +0530

    refactor: shift fedora to folder
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

---
+++ fedora/.zshrc
@@ -0,0 +1,56 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=2000
+SAVEHIST=5000
+setopt autocd beep extendedglob
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/king-11/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+export PATH=/home/king-11/.fnm:$PATH
+eval "`fnm env`"
+
+export GOPATH=$HOME/.go
+
+export BROWSER=/usr/bin/brave-browser
+
+alias ls='exa --icons'
+alias cat='bat'
+alias grep='rg'
+alias cp="cp -i"
+alias df="df -h"
+alias ghs="ssh-add $HOME/.ssh/github_rsa &>/dev/null"
+
+bindkey '^[[H' beginning-of-line
+bindkey '^[OH' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey '^[OF' end-of-line
+
+eval "$(starship init zsh)"
+eval "$(ssh-agent -s)" &>/dev/null
+ssh-add $HOME/.ssh/github_rsa &>/dev/null
+
+source ~/.zplug/init.zsh
+
+zplug "plugins/zsh-interactive-cd",   from:oh-my-zsh
+zplug "plugins/colored-man-pages",   from:oh-my-zsh
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-autosuggestions", defer:2
+
+zplug load
+
+# pnpm
+export PNPM_HOME="/home/king-11/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end
---
+++ fedora/fedora.sh
@@ -0,0 +1,96 @@
+functio initial_setup(){
+  sudo dnf check-update
+  sudo dnf makecache --refresh
+  sudo dnf upgrade
+  echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf\
+  echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf\
+  echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+  ibus-setup #remove Control Period from emoji to make quick fix shortcut work
+
+  sudo dnf install tlp tlp-rdw
+  sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
+  sudo systemctl enable --now tlp.service
+  sudo dnf remove power-profiles-daemon
+}
+
+function time_setup(){
+  timedatectl set-local-rtc 1
+}
+
+function rpm_fusion(){
+  sudo dnf install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  sudo dnf install   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+ 
+}
+
+function git_install(){
+  sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+  sudo dnf install gh
+  gpg --import ~/.ssh/gpg-github.asc
+  #gpg --edit-key <LONGID> ; gpg --list-secret-keys --keyid-format LONG ;copy sec second line
+}
+
+function terminal(){
+  sudo dnf install zsh chsh
+  chsh -s $(which zsh)
+  bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+  sudo dnf install hyper powerline-fonts -y
+}
+
+function nvidia(){
+  sudo dnf install akmod-nvidia
+}
+
+function dev_packages(){
+  sudo dnf -y install gdb gcc-c++ clang make automake gcc kernel-devel ffmpeg dnf-plugins-core
+  sudo dnf group install "C Development Tools and Libraries"
+}
+
+function applications(){
+  sudo dnf install vlc code
+  sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+  sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+  sudo dnf install sublime-text neovim python3-neovim -y
+}
+
+function python(){
+  sudo dnf install --assumeyes python3-pip
+  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | zsh
+}
+
+function nodejs(){
+  curl -fsSL https://fnm.vercel.app/install | zsh
+  curl -fsSL https://get.pnpm.io/install.sh | sh -
+}
+
+function brave(){
+  sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+  sudo dnf install brave-browser
+}
+
+function social_media(){
+  sudo dnf copr enable rommon/telegram
+  sudo dnf install telegram-desktop
+  sudo dnf copr enable tcg/discord
+  sudo dnf install discord
+}
+
+function utilites(){
+  sudo dnf install flatpak snapd
+  sudo dnf install bat stacer procs ripgrep exa duf
+  sudo dnf copr enable elxreno/preload && sudo dnf install preload -y
+}
+
+function languages() {
+  sudo dnf install golang
+  mkdir -p $HOME/.go
+  echo 'export GOPATH=$HOME/.go' >> $HOME/.zshrc
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+function firmware() {
+  sudo fwupdmgr refresh --force
+  sudo fwupdmgr get-updates
+  sudo fwupdmgr update
+}
