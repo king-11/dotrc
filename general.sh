41dba8019760f21b9002c523531a75058f15ae15
Lakshya Singh <lakshay.singh1108@gmail.com>
2023-07-27 22:33:54 +0530

    feat: move common pkgs
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- general.sh
+++ general.sh
@@ -1,32 +1,94 @@
-function create_gpg(){
+function create_gpg() {
   gpg --full-generate-key #4096 size
 }
 
-function list_gpg(){
+function list_gpg() {
   gpg --list-secret-keys --keyid-format=long
 }
 
-function export_gpg_key(){
+function export_gpg_key() {
   gpg --export-secret-keys <LONGID> > private.key
 }
 
-function edit_gpg(){
+function edit_gpg() {
   gpg --edit-key <LONGID>
 }
 
-function gpg_import(){
+function gpg_import() {
   gpg --import ~/.ssh/gpg-github.asc
 }
 
-function generate_ssh(){
+function generate_ssh() {
   ssh-keygen -t ed25519 -C "lakshay.singh1108@gmail.com"
 }
 
-function add_ssh_key(){
+function add_ssh_key() {
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/github_rsa
 }
 
-function time_set(){
+function time_set() {
   timedatectl set-local-rtc 1
+}
+
+function config_terminal() {
+  chsh -s $(which zsh)
+  bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+}
+
+function rust_install() {
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  
+}
+
+function fnm_install(){
+  curl -fsSL https://fnm.vercel.app/install | zsh
+}
+
+function nodejs_install() {
+  fnm install v18
+  fnm default v18
+}
+
+function pnpm_install(){
+  curl -fsSL https://get.pnpm.io/install.sh | sh -
+  corepack enable
+}
+
+function python_install() {
+  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | zsh
+  pyenv global 3.8
+}
+
+function poetry_install() {
+  curl -sSL https://install.python-poetry.org | python3 -
+}
+
+function poetry_update(){
+  poetry self update
+}
+
+function zinit_update(){
+  zinit self update
+  zinit update --all
+}
+
+function zinit_completions(){
+  zinit cclear
+  zinit creinstall zsh-users/zsh-completions
+}
+
+function brew_install() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+function brew_install_packages() {
+  brew install bottom bat ripgrep exa procs dust duf
+  brew install difftastic
+  brew install zellij
+  brew install nushell
+  brew install teaxyz/pkgs/tea-cli
+}
+
+function install_tlp_ui() {
+  flatpak install flathub tlpui
 }
